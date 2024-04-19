b908ceab45615350adc11c08a8906c9b18b3dc9e
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-04-19 14:31:40 -0300

    Metodo do cadastro feito

--- projeto/App.java
+++ projeto/App.java
@@ -1,7 +1,4 @@
-package CriandoUmGrafo.projeto;
-
 public class App {
     public static void main(String[] args) {
-        
     }
 }
--- projeto/elementos/Rede.java
+++ projeto/elementos/Rede.java
@@ -1,15 +1,49 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Rede {
-    private HashMap<Usuario, Interger> UsuariosDaRede;
+  
+    private HashMap<Usuario, Integer> usuariosDaRede = new HashMap<Usuario, Integer>();
 
-    public void cadrastar(String nome, String senha){
+    public void cadastarUsuario(){
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean valido = false;
+
+        while(valido == false)
+            System.out.print("Digite seu novo nome de login: ");
+            String nome = scanner.nextLine();
+
+            if ("".equals(nome) ||  nome == null){
+                Exception NomeInvalido = new Exception("Você deve digitar um nome válido.");
+                throw NomeInvalido;
+            }
+
+            if(usuariosDaRede.size() > 0){
+                for(Usuario usuario : usuariosDaRede.keySet()){
+                    if(usuario.getNome().equals(nome)){
+                        System.out.printf("Usuário %s já existe. Tente novamente.", usuario.getNome());
+                        break;
+                    }
+                }
+            }
+            valido = true;
+    
+        System.out.print("Digite sua senha: ");
+        String senha = scanner.nextLine();
+        
+        Usuario usuario = new Usuario(nome, senha);
+        usuariosDaRede.put(usuario, usuariosDaRede.size());
+
+        System.out.println("Seu cadrasto foi criado com sucesso");
+        scanner.close();
+    }
+
+    public void fazerLoginDoUsuario(){
 
     }
-    public void fazerLogin(){
 
-    }
-    public void recomendar(){
+    public void recomendarAmigos(){
 
     }
 }
