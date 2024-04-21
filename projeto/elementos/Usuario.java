577ecb047f17b94392c47d61d5e938d5aea4c753
Sarinho <yasmiinsarinho@gmail.com>
2024-04-21 08:03:04 -0300

    classe usuario corrigida

--- projeto/elementos/Usuario.java
+++ projeto/elementos/Usuario.java
@@ -28,11 +28,29 @@ public class Usuario {
     }
 
     public void adicionarAmigo(String nomeAdicionar){
-
-        if (verificarExistenciaDeUsuario(nomeAdicionar)){
-            
+        if (verificarExistenciaDeUsuario(nomeAdicionar) && verificarExistenciaDeAmizade(nomeAdicionar)){
+            for (Usuario usuario : Rede.getUsuariosDaRede().keySet()){
+                if (nome.equals(usuario.getNome())){
+                    listaDeAmigos.add(usuario);
+                    System.out.println("Amigo adicionado!");
+                }
+            }
         }
+    }
 
+    public void excluirAmigo(String nomeRemover){
+        if (verificarExistenciaDeUsuario(nomeRemover) && verificarExistenciaDeAmizade(nomeRemover) == false){
+
+        }
+    }
+
+    public boolean verificarExistenciaDeAmizade(String nome){
+        for(Usuario amigo : listaDeAmigos){
+            if (amigo.equals(nome)){
+                return false;
+            }
+        }
+        return true;
     }
 
     public boolean verificarExistenciaDeUsuario(String nome) {
