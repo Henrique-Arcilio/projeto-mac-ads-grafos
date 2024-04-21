66945bb0b3788786f6d18b0a1543d086bc88a88d
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-04-21 20:59:17 -0300

    Metodo criarMatriz()

--- projeto/elementos/MatrizAdjacencia.java
+++ projeto/elementos/MatrizAdjacencia.java
@@ -1,3 +1,24 @@
+import java.util.HashMap;
+
 public class MatrizAdjacencia {
-    private int[][] matriz;
+
+    public int[][] criarMatriz(){
+        HashMap<Integer, Usuario> listaUsuarios = Rede.getUsuariosDaRede();
+
+        int quantia = listaUsuarios.size();
+        int[][] matriz = new int[quantia][quantia];
+        for(int i = 0; i < quantia; i++){
+            for(int j = 0; j < quantia; j++){
+                Usuario usuario = listaUsuarios.get(i);
+                Usuario amigo = listaUsuarios.get(j);
+                if((usuario.verificarExistenciaDeAmizade(amigo.getNome()))){
+                    matriz[i][j] = 1;
+                }
+                else{
+                    matriz[i][j] = 0;
+                } 
+            }
+        }
+        return matriz;
+    }
 }
