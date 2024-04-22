67a975e079e926bf31d1c193b2179e67ef79ae83
Henrique-Arcilio <aldomario.arcilio@outlook.com>
2024-04-22 13:03:20 -0300

    Pequeno teste das funcionalidades

--- projeto/App.java
+++ projeto/App.java
@@ -1,5 +1,46 @@
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) {
-        
+        try {
+            MatrizAdjacencia matriz = new MatrizAdjacencia();
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+            Rede.cadastrarUsuario();
+            System.out.println(Rede.getUsuariosDaRede().entrySet());
+
+            Rede.getUsuariosDaRede().get(0).adicionarAmigo("jonas");
+            Rede.getUsuariosDaRede().get(0).adicionarAmigo("kleber");
+            Rede.getUsuariosDaRede().get(0).adicionarAmigo("chico");
+            Rede.getUsuariosDaRede().get(1).adicionarAmigo("jonas");
+            Rede.getUsuariosDaRede().get(1).adicionarAmigo("kleber");
+            Rede.getUsuariosDaRede().get(1).adicionarAmigo("chico");
+            Rede.getUsuariosDaRede().get(5).adicionarAmigo("kleber");
+            Usuario user = Rede.getUsuariosDaRede().get(0);
+            System.out.println(Rede.recomendarAmigos(user));
+
+
+
+
+            
+            int [][] matriza = matriz.criarMatriz();
+            for (int i = 0; i < matriza.length; i++) {
+                for (int j = 0; j < matriza[i].length; j++) {
+                    System.out.print(matriza[i][j] + " ");
+                }
+                System.out.println(); // Pula para a próxima linha após imprimir uma linha completa
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
